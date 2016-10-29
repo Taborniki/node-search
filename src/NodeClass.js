924b794fe8bec1352559a052948dc4d9d4f8171c
Nero <louis.vanbiervliet@gmail.com>
2016-10-29 22:46:00 +0200

    working zoom and shadow demo

--- src/NodeClass.js
+++ src/NodeClass.js
@@ -3,19 +3,57 @@ function Node (parentNode, iconUrl, title) {
     this.iconUrl = iconUrl;
     this.title = title;
     this.visual = this.createVisual();
+    this.childNodes = [];
+
+    // add this node as a child to the parentNode
+    if(parentNode != 'none') {
+        parentNode.addChild(this);
+    }
 }
 
-
-// returns the parent Node
-Node.prototype.getParentNode = function() {
-    return this.parentNode;
+// adds a child node
+Node.prototype.addChild = function(childNode) {
+    this.childNodes.push(childNode);
 };
 
 // creates the visual html node
 Node.prototype.createVisual = function() {
 
-    // icon inladen en resizen
-    // schaduw rondje inladen
+    // NEED icon inladen en resizen
+    // NEED schaduwig rondje inladen
+    var image = new Image();
+    image.src = this.iconUrl;
+    var bitmap = new createjs.Bitmap(image);
 
-    return this.parentNode;
+    bitmap.regX = bitmap.image.width / 2;
+    bitmap.regY = bitmap.image.height / 2;
+    bitmap.scaleX = bitmap.scaleY = bitmap.scale = 0.8;
+
+    // shadow
+    bitmap.shadow = new createjs.Shadow("#0094FF", 0,0, 0);
+
+    // hover animation
+    bitmap.on("mouseover", function (evt) {
+        // enlarge
+        createjs.Tween.get(this, {loop: false})
+            .to({scaleX: 1, scaleY: 1}, 200, createjs.Ease.quartInOut);
+        // add shadow
+        createjs.Tween.get(this.shadow, {loop:false})
+            .to({blur:15}, 200, createjs.Ease.quartInOut);
+
+
+        update = true;
+    });
+    bitmap.on("rollout", function (evt) {
+        // make smaller
+        var tween = createjs.Tween.get(this, {loop: false})
+            .to({scaleX: 0.8, scaleY: 0.8}, 100, createjs.Ease.quartInOut);
+        // remove shadow
+        createjs.Tween.get(this.shadow, {loop:false})
+            .to({blur:0}, 200, createjs.Ease.quartInOut);    
+
+        update = true;
+    });
+
+    return bitmap;
 };
--- src/main.js
+++ src/main.js
@@ -4,12 +4,18 @@ var dragStarted;	// indicates whether we are currently in a drag operation
 var offset;
 var update = true;
 var images = [];
+var rootNode;
+
+var defaultRectangle = new createjs.Shape();
+defaultRectangle.graphics.beginFill("white").drawRect(0, 0, 100, 6); // .beginStroke("red")
+
 
 $(document).ready(function() {
     init();
 });
 
 function init() {
+
     // create stage and point it to the canvas:
     canvas = document.getElementById("testCanvas");
     stage = new createjs.Stage(canvas);
@@ -31,6 +37,75 @@ function init() {
     image.src = "images/nodes/wiki.png";
     images.push(image);
     image.onload = handleImageLoad;
+
+    createTestTree();
+    drawTree(rootNode);
+}
+
+function createTestTree() {
+    // root node 0.1
+    rootNode = new Node('none','images/nodes/google.png','world war II');
+
+    // 1.1
+    var wikiNode = new Node(rootNode, 'images/nodes/wiki.png','second world war');
+
+    // 1.2
+    var wikiNode2 = new Node(rootNode, 'images/nodes/wiki.png','nazi germany');
+
+    // 1.1.1
+    var wikiNode3 = new Node(wikiNode, 'images/nodes/wiki.png','first wirld war');
+
+    // 1.1.2
+    var bbcNode = new Node(wikiNode, 'images/nodes/bbc.png', 'second world war');
+}
+
+function drawTree (node) {
+
+    var posX;
+    var posY;
+
+    // in pixels
+    var HORIZONTAL_NODE_SPACING = 120;
+    var VERTICAL_NODE_SPACING = 120;
+
+    if(node.parentNode == 'none') {
+        node.visual.x = 400;
+        node.visual.y = 50;
+    }
+    else {
+        numSiblings = node.parentNode.childNodes.length-1;
+
+        if(numSiblings === 0) {
+            // right beneath parent node
+            node.visual.x = node.parentNode.visual.x;
+        }
+        else {
+            // not straight below parent node
+            var childRank = node.parentNode.childNodes.indexOf(node);
+            node.visual.x = node.parentNode.visual.x - HORIZONTAL_NODE_SPACING/2*numSiblings + childRank*HORIZONTAL_NODE_SPACING;
+        }
+
+        // below parent node
+        node.visual.y = node.parentNode.visual.y + VERTICAL_NODE_SPACING;
+    }
+
+    // add node to canvas NEED
+    stage.addChild(node.visual);
+
+    // draw the child nodes
+    for(var i=0; i<node.childNodes.length; i++) {
+        drawTree(node.childNodes[i]);
+    }
+}
+
+function drawLine(pos1, pos2) {
+    var line = defaultRectangle.clone(true);
+    line.regX = 100/2;
+    line.regY = 6/2;
+    line.rotation = 45;
+    line.x = 500;
+    line.y = 200;
+    stage.addChild(recto);
 }
 
 function handleImageLoad(event) {
@@ -39,7 +114,7 @@ function handleImageLoad(event) {
     stage.addChild(container);
 
     // create and populate the screen with random daisies:
-    for (var i = 0; i < 5; i++) {
+    for (var i = 0; i < 0; i++) { // NEED disabled atm door i<0
         var randindex = Math.floor(Math.random()*images.length);
         var image = images[randindex];
         var bitmap;
@@ -78,8 +153,7 @@ function handleImageLoad(event) {
     }
 
     // rectangle test
-    var recto = new createjs.Shape();
-    recto.graphics.beginFill("white").drawRect(0, 0, 100, 6); // .beginStroke("red")
+    var recto = defaultRectangle.clone(true);
     recto.regX = 100/2;
     recto.regY = 6/2;
     recto.rotation = 45;
@@ -92,7 +166,7 @@ function handleImageLoad(event) {
 function tick(event) {
     // this set makes it so the stage only re-renders when an event handler indicates a change has happened.
     if (update) {
-        update = false; // only update once
+        update = true; // only update once
         stage.update(event);
     }
 }
